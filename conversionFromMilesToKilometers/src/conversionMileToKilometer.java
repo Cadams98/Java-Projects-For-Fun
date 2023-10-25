1d0c2850cfa6a60c87adba7b574931bdc6bfcf4b
Cadams98 <christianadams98@icloud.com>
2023-10-25 18:21:09 -0400

    Renamed the Class.

--- conversionFromMilesToKilometers/src/conversionMileToKilometer.java
+++ conversionFromMilesToKilometers/src/conversionMileToKilometer.java
@@ -1,4 +1,4 @@
-public class App {
+public class conversionMileToKilometer {
     public static void main(String[] args) throws Exception {
         
         System.out.println("Miles           Kilometers");
